508dcd25676e4bca72930aee85115e9c21c185c4
Saurabh <singhalsaurabh95@gmail.com>
2015-11-01 13:03:38 +0530

    Update SendData.php

--- PHP/SendData.php
+++ PHP/SendData.php
@@ -1,18 +1,29 @@
 <?php
-include('/db.php');
+require_once('config.php');
 $json = file_get_contents('php://input');
+$conn=mysqli_connect(SERVER,USER,PASS,DB) or die("Connection error");
 $response= json_decode($json,true);
 date_default_timezone_set("Asia/Kolkata");
-$class=$response['class'];
-$sem=$response['sem'];
-$tablename=date("_d_m_Y_h_i_").$sem.$class;
-$query="create table ".$tablename." (roll_no varchar(11) primary key,present boolean not null)";
-mysqli_query($conn,$query) or die (mysqli_error($conn));
+$class=mysqli_real_escape_string($conn,$response['class']);
+$sem=mysqli_real_escape_string($conn,$response['sem']);
+$tablename=$class.'_'.$sem.'_'.date("m_Y");
 $data=$response['data'];
+// $tablename='tablename';
+// $data = array('10313202712' => true,'10413202712' => true,'10513202712' => true,'10613202712' => false,'10713202712' => true,'10813202712' => false );
+$query='create table if not exists '.$tablename.' ( day int primary key';
+	foreach ($data as $key => $value) {
+		$query.=',_'.$key.' boolean not null default false';
+	}
+	$query.=')';
+echo $query.'<br>';
+mysqli_query($conn,$query);
+$query='insert into '.$tablename. ' values ('.date('d');
 foreach ($data as $key => $value) {
-	$value=$value?1:0;
-	$query='insert into '.$tablename.' values (\''.$key.'\','.$value.')';
-	mysqli_query($conn,$query);
-}
+	$boolval=$value?1:0;
+		$query.=','.$boolval;
+	}
+	$query.=')';
+echo $query;
+mysqli_query($conn,$query);
 mysqli_close($conn);
 ?>
